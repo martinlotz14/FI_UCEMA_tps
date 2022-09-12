1501725849facfa1662c4f3d2013d1756ad438d0
Martin Lotz <martinlotz44@gmail.com>
2022-09-12 20:16:45 -0300

    subiendo practica exepciones

---
+++ practica_manejo_exepciones/practica_exepciones.py
@@ -0,0 +1,26 @@
+#ejercicio 1
+"si aunque le agregaria el tipo de error que estamos buscando, el tipo de error que puede llegar a ocurrir. Por ejemplo si el problema  fuera una posible division por cero pondria except ZeroDivisionError"
+
+#ejercicio 2
+"""
+lista = [2, 6, 10, 14]
+lista2 = []
+divisor = int(input("ingrese el numero por el cual quiere dividir:"))
+try:
+    resultado= lista/divisor
+    lista2.append(resultado)
+    print(lista2)
+
+except ZeroDivisionError:
+    print("no se puede dividir por 0")
+"""
+
+#ejercicio3
+"""
+lista = []
+numero = int(input("ingresar un numero:"))
+if numero > 0 :
+    lista.append(numero)
+else:
+    print(TypeError)
+"""
